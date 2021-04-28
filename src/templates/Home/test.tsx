e869a5221803e7f4223c969f89d77760b28de7d5
Joao Moraes <joaovitordmoraes@gmail.com>
2021-04-28 11:59:52 -0300

    Add home styles and tests

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,13 +1,57 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
 import Home from '.'
 
+const props = {
+  banners: bannersMock,
+  newGames: gamesMock,
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: gamesMock,
+  upcommingGames: gamesMock,
+  upcommingHighlight: highlightMock,
+  upcommingMoreGames: gamesMock,
+  freeGames: gamesMock,
+  freeHighlight: highlightMock
+}
+
 describe('<Home />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Home />)
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render sections', () => {
+    renderWithTheme(<Home {...props} />)
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render section elements', () => {
+    renderWithTheme(<Home {...props} />)
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+    // highlight
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
