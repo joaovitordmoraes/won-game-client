910c4758ec47bc2036bad2fc4af2deb1fbc69c85
Joao Moraes <joaovitordmoraes@gmail.com>
2021-04-28 14:26:50 -0300

    Optimize Home tests

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -10,13 +10,13 @@ import Home from '.'
 
 const props = {
   banners: bannersMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcommingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
   upcommingHighlight: highlightMock,
-  upcommingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighlight: highlightMock
 }
 
@@ -24,15 +24,15 @@ describe('<Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />)
 
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    // footer
     expect(
       screen.getByRole('heading', { name: /follow us/i })
     ).toBeInTheDocument()
     expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
-  })
 
-  it('should render sections', () => {
-    renderWithTheme(<Home {...props} />)
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /most popular/i })
@@ -43,14 +43,11 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render section elements', () => {
-    renderWithTheme(<Home {...props} />)
     // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
     // highlight
     expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
