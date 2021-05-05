6b4cce1a0240dbb9503103167da48ca483ea0bb9
Joao Moraes <joaovitordmoraes@gmail.com>
2021-05-05 11:29:08 -0300

    Create labelColor in Checkbox component

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Checkbox from '.'
@@ -10,5 +11,21 @@ describe('<Checkbox />', () => {
     expect(screen.getByRole('checkbox')).toBeInTheDocument()
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without a label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
   })
 })
