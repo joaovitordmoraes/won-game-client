9e76768b04adb354ac8e91a4ccbf33801c7259f8
Joao Moraes <joaovitordmoraes@gmail.com>
2021-06-09 12:00:28 -0300

    Create Base template and refactor Home and tests

---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,27 @@
+import Menu from 'components/Menu'
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+
+import * as S from './styles'
+
+export type BaseTemplateProps = {
+  children: React.ReactNode
+}
+
+const Base = ({ children }: BaseTemplateProps) => (
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    {children}
+
+    <S.SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </S.SectionFooter>
+  </section>
+)
+
+export default Base
