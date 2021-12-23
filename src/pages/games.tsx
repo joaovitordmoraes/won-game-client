5311340fcd6d2e03d567e89c3e08930586b53a72
Joao Moraes <joaovitordmoraes@gmail.com>
2021-12-23 12:48:35 -0300

    Create Games page

---
+++ src/pages/games.tsx
@@ -0,0 +1,17 @@
+import { GamesTemplateProps } from 'templates/Games'
+import GamesTemplate from 'templates/Games'
+import filterItemsMock from 'components/ExploreSidebar/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+
+export default function GamesPage(props: GamesTemplateProps) {
+  return <GamesTemplate {...props} />
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      games: gamesMock,
+      filterItems: filterItemsMock
+    }
+  }
+}
