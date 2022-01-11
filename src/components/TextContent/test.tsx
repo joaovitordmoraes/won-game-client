764f5810d96ca85c3d22b7054590b70d2bd97648
Joao Moraes <joaovitordmoraes@gmail.com>
2022-01-10 22:55:19 -0300

    Prettier fix

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -32,8 +32,9 @@ describe('<TextContent />', () => {
   it('should render colors correctly on screen sizes', () => {
     renderWithTheme(<TextContent {...props} />)
 
-    const wrapper = screen.getByRole('heading', { name: /description/i })
-      .parentElement
+    const wrapper = screen.getByRole('heading', {
+      name: /description/i
+    }).parentElement
 
     expect(wrapper).toHaveStyle({
       color: '#FAFAFA'
