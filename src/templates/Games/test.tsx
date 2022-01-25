a81487c1841cd7c5900a1db3a0df84dd20fa699d
Joao Moraes <joaovitordmoraes@gmail.com>
2022-01-24 23:05:08 -0300

    Add search logic in explore page

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -8,6 +8,17 @@ import Games from '.'
 import userEvent from '@testing-library/user-event'
 import apolloCache from 'utils/apolloCache'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
+
 jest.mock('templates/Base', () => {
   return {
     __esModule: true,
@@ -17,14 +28,12 @@ jest.mock('templates/Base', () => {
   }
 })
 
-jest.mock('components/ExploreSidebar', () => {
-  return {
-    __esModule: true,
-    default: function Mock({ children }: { children: React.ReactNode }) {
-      return <div data-testid="Mock ExploreSidebar">{children}</div>
-    }
+jest.mock('next/link', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div>{children}</div>
   }
-})
+}))
 
 describe('<Games />', () => {
   it('should render loading when starting the template', () => {
@@ -49,7 +58,7 @@ describe('<Games />', () => {
     expect(screen.getByText(/loading.../i)).toBeInTheDocument()
 
     //we wait until we have data to get the elements
-    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
+    expect(await screen.findByText(/Price/i)).toBeInTheDocument()
 
     expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
@@ -68,5 +77,22 @@ describe('<Games />', () => {
     userEvent.click(await screen.findByRole('button', { name: /show more/i }))
 
     expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
+  })
+
+  it('should change push router when selecting a filter', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
+    userEvent.click(await screen.findByRole('checkbox', { name: /linux/i }))
+    userEvent.click(await screen.findByLabelText(/low to high/i))
+
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/games',
+      query: { platforms: ['windows', 'linux'], sort_by: 'low-to-high' }
+    })
   })
 })
