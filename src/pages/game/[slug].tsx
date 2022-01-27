b88c4343dc57c99b494fe773315ef030c2222fc8
Joao Moraes <joaovitordmoraes@gmail.com>
2022-01-27 10:21:26 -0300

    Fix revalidate and fetchPolicy in home and game page

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -52,7 +52,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     QueryGameBySlugVariables
   >({
     query: QUERY_GAME_BY_SLUG,
-    variables: { slug: `${params?.slug}` }
+    variables: { slug: `${params?.slug}` },
+    fetchPolicy: 'no-cache'
   })
 
   if (!data.games.length) {
@@ -75,8 +76,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   })
 
   return {
+    revalidate: 60,
     props: {
-      revalidate: 60,
       cover: `${game.cover?.src}`,
       gameInfo: {
         title: game.name,
