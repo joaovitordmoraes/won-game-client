de83ecb5894f8bc5fbd9128451a5c082c0575d42
Joao Moraes <joaovitordmoraes@gmail.com>
2022-01-27 15:42:20 -0300

    Create mapper tests

---
+++ src/utils/mappers/test.ts
@@ -0,0 +1,106 @@
+import { QueryGames_games } from 'graphql/generated/QueryGames'
+import {
+  QueryHome_banners,
+  QueryHome_sections_freeGames_highlight
+} from 'graphql/generated/QueryHome'
+import { bannerMapper, gamesMapper, highlightMapper } from '.'
+
+describe('bannerMapper()', () => {
+  it('should return the correct format when mapped', () => {
+    const banner = {
+      image: {
+        url: '/image.jpg'
+      },
+      title: 'Banner Title',
+      subtitle: 'Banner subtitle',
+      button: {
+        label: 'button label',
+        link: 'button link'
+      },
+      ribbon: {
+        text: 'ribbon text',
+        color: 'primary',
+        size: 'small'
+      }
+    } as QueryHome_banners
+
+    expect(bannerMapper([banner])).toStrictEqual([
+      {
+        img: '/image.jpg',
+        title: 'Banner Title',
+        subtitle: 'Banner subtitle',
+        buttonLabel: 'button label',
+        buttonLink: 'button link',
+        ribbon: 'ribbon text',
+        ribbonColor: 'primary',
+        ribbonSize: 'small'
+      }
+    ])
+  })
+})
+
+describe('gamesMapper()', () => {
+  it('should return an empty array in there are no games', () => {
+    expect(gamesMapper(null)).toStrictEqual([])
+  })
+
+  it('should return the correct format when mapped', () => {
+    const game = {
+      id: '1',
+      name: 'game',
+      developers: [
+        {
+          name: 'developer'
+        }
+      ],
+      slug: 'game',
+      cover: {
+        url: '/image.jpg'
+      },
+      price: 10
+    } as QueryGames_games
+
+    expect(gamesMapper([game])).toStrictEqual([
+      {
+        id: '1',
+        title: 'game',
+        slug: 'game',
+        developer: 'developer',
+        img: '/image.jpg',
+        price: 10
+      }
+    ])
+  })
+})
+
+describe('highlightMapper()', () => {
+  it('should return an empty object if no highlight', () => {
+    expect(highlightMapper(null)).toStrictEqual({})
+  })
+
+  it('should return mapped highlight', () => {
+    const highlight = {
+      title: 'title',
+      subtitle: 'subtitle',
+      background: {
+        url: '/image.jpg'
+      },
+      floatImage: {
+        url: '/image.jpg'
+      },
+      buttonLabel: 'button label',
+      buttonLink: 'button link',
+      alignment: 'right'
+    } as QueryHome_sections_freeGames_highlight
+
+    expect(highlightMapper(highlight)).toStrictEqual({
+      title: 'title',
+      subtitle: 'subtitle',
+      backgroundImage: '/image.jpg',
+      floatImage: '/image.jpg',
+      buttonLabel: 'button label',
+      buttonLink: 'button link',
+      alignment: 'right'
+    })
+  })
+})
